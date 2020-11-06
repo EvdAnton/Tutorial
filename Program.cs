64183be899289a88549d83c4e869fced42f049ae
Anton Evdakov <aevdakov@AM.IDTCORP.NET>
2020-11-07 01:12:16 +0300

    [Tch] fix(comments): Add some comments to task implementation

--- Program.cs
+++ Program.cs
@@ -1,77 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-namespace third_class
+namespace third_class // never call namespace like this)
+// use PascalCase
 {
     class Program
     {
-        public static void Main() {
+        public static void Main()
+        {
 
-            int[] answer;
+            // So u can divide your program into 3 parts
+            // Initializing
+            // Get Prime Factors
+            // Get Sum
+            // ok and output
 
-            int n = int.Parse(Console.ReadLine());
+            // i think it's will be better if u put all this parts in separate private methods
 
-            int[] arr = new int[n];
+            int[] answer; // and why is it here? if u use it on 21 line only
 
-            for (int i = 0; i < n; i++)
+            var n = int.Parse(Console.ReadLine()); // naming problem, use var
+
+            var arr = new int[n]; // arr is not display sense of variable, use var
+
+            for (var i = 0; i < n; i++) // use var
             {
                 arr[i] = int.Parse(Console.ReadLine());
             }
 
-            answer = stringOfPrimeFactors(arr).ToArray();
+            answer = stringOfPrimeFactors(arr).ToArray(); // maid be it's easier to use List instead of array?  
 
-            answer.OrderBy(i => i);
+            answer.OrderBy(i => i); // Order return result, but u dont use it
 
-            answer = answer.Distinct().ToArray();
+            answer = answer.Distinct().ToArray(); // u twice use method ToArray(), it is necessary ?
 
-            string ANSWER = "";
+            string ANSWER = ""; // use camelCase for variable and fields
 
-            for (int i = 0; i < answer.Length; i++){
+            // u can use Linq to find Sum of elements -> 
+
+            // answer.Select(key => arr.Where(element => element % key == 0).Sum());
+
+            for (int i = 0; i < answer.Length; i++)
+            { // foreach is better here
 
                 int tempSum = 0;
 
-                for (int k = 0; k < n; k++) {
+                for (int k = 0; k < n; k++)
+                { // foreach is better to (foreach (var element in arr))
 
-                    if (arr[k] % answer[i] == 0) {
+                    if (arr[k] % answer[i] == 0)
+                    {
 
-                        tempSum += arr[k];      
+                        tempSum += arr[k];
                     }
                 }
 
-                ANSWER += $"({answer[i]} {tempSum}) ";
+                ANSWER += $"({answer[i]} {tempSum}) "; // in this case u create new string every time, when u rewrite it
+                // instead it use StringBuilder class 
             }
 
-			Console.WriteLine(ANSWER);
+            Console.WriteLine(ANSWER);
         }
 
-        public static List<int> stringOfPrimeFactors(int[] list)
+        public static List<int> stringOfPrimeFactors(int[] list /* why array is list?)*/)
+        // why u use public if it's only part of functionality
+        // please, always use Pascalcase for method
         {
 
-            int size = list.Length;
+            var size = list.Length; // use var
 
-            int[] copy = new int[size];
+            var copy = new int[size]; // if is like helper variable for using, often in programs is named like 'temp' or 'tempSequence'
 
-            Array.Copy(list, copy, size);
-                     
-            List<int> primes = new List<int>();
+            Array.Copy(list, copy, size); // u can use list.CopyTo(copy, 0);
 
-            for (int i = 0; i < size; i++)
-            {   
-                for (int k = 2; k <= Math.Abs(copy[i]); k++)
+            List<int> primes = new List<int>(); // use var
+
+            for (int i = 0; i < size; i++) // u can use foreach here
+            {
+                for (int k = 2; k <= Math.Abs(copy[i]); k++) // we can change Math.Abs(copy[i]) on Math.Sqrt(Math.Abs(copy[i]))
                 {
-                    while (copy[i] % k == 0) {
-                       
-                        copy[i] /= k;
-                    }
-                    if (list[i] % k == 0) {
-
-                    primes.Add(k);
-                    }  
-                }
+                    while (copy[i] % k == 0)
+                    { // so u can use something like this here -->           if(copy[i] % k == 0)
+                      // in this case we don't touch old array at all        {
+                        copy[i] /= k;          //                                                         do                               
+                    }                          //                                                         { 
+                    if (list[i] % k == 0)
+                    {    //                                                             copy[i] /= k;
+                         //                                                         }while(copy[i] % k == 0)                                                                
+                        primes.Add(k);         //                                                         
+                    }                          //                                                         primes.Add(k);   
+                }                              //                                                     }                 
             }
             return primes;
         }
-      
+
     }
 }
-
